9acb93dd6df499a4252ec9448cb3b74934694dbf
Alexandra Ashcraft <120073483+AlexandraAshcraft@users.noreply.github.com>
2023-10-30 10:37:02 -0700

    Initial commit

---
+++ src/client/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './assets/App.css';
+import MainContainer from './containers/MainContainer';
+
+function App() {
+  return (
+    <>
+      <MainContainer />
+    </>
+  );
+}
+
+export default App;
